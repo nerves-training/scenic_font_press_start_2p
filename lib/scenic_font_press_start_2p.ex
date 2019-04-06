21ebd300b6f12afa93713e927c524dbdb7964c59
Justin Schneck <jschneck@mac.com>
2019-04-06 13:07:45 -0400

    Update for Scenic 0.10

--- lib/scenic_font_press_start_2p.ex
+++ lib/scenic_font_press_start_2p.ex
@@ -1,18 +1,24 @@
 defmodule ScenicFontPressStart2p do
-  @hash Application.app_dir(:scenic_font_press_start_2p, ["priv", "PressStart2P.ttf"])
-        |> Scenic.Cache.Hash.file!(:sha)
+  @font_hash "eLcVQnSbL2kg9g_ix0_oVzgoTcuc0mp_9qZCxU04Rws"
+  @font_metrics_hash  :code.priv_dir(:scenic_font_press_start_2p)
+    |> Path.join("PressStart2P.ttf.metrics")
+    |> Scenic.Cache.Support.Hash.file!(:sha)
 
   def load() do
-    Scenic.Cache.File.load(path(), hash())
-    Scenic.Cache.File.read(path(), hash())
+    Scenic.Cache.Static.FontMetrics.load(font_metrics(), @font_metrics_hash)
+    Scenic.Cache.Static.Font.load(font_folder(), @font_hash)
     :ok
   end
 
-  def path() do
-    Application.app_dir(:scenic_font_press_start_2p, ["priv", "PressStart2P.ttf"])
+  def hash() do
+    @font_metrics_hash
   end
 
-  def hash() do
-    @hash
+  def font_folder() do
+    :code.priv_dir(:scenic_font_press_start_2p) |> to_string()
+  end
+
+  def font_metrics() do
+    :code.priv_dir(:scenic_font_press_start_2p) |> Path.join("PressStart2P.ttf.metrics")
   end
 end
