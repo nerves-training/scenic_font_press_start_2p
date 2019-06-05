65912de96c8c5a046a9764a22d9c0c6116c3294f
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-04 22:42:19 -0400

    Add license

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule ScenicFontPressStart2p.MixProject do
       app: :scenic_font_press_start_2p,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,5 +26,16 @@ defmodule ScenicFontPressStart2p.MixProject do
       {:scenic, "~> 0.10"},
       {:truetype_metrics, "~> 0.3"}
     ]
+  end
+
+  defp description do
+    "Press Start 2P Font for Scenic"
+  end
+
+  defp package do
+    %{
+      licenses: ["OFL-1.1"],
+      links: %{"GitHub" => "https://github.com/nerves-training/scenic_font_press_start_2p"}
+    }
   end
 end
