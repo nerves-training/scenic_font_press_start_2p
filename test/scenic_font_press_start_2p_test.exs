c2aa5bbab695e84b2cc17e58d5933a470dd043de
Frank Hunleth <fhunleth@troodon-software.com>
2020-02-24 18:39:13 -0500

    Remove unused files

--- test/scenic_font_press_start_2p_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ScenicFontPressStart2pTest do
-  use ExUnit.Case
-  doctest ScenicFontPressStart2p
-
-  test "greets the world" do
-    assert ScenicFontPressStart2p.hello() == :world
-  end
-end
